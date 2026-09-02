20a8cbbf4e1e4fdc7c9bdd79ff55ffb37ce61d3e
24nycolas1b <nycolas.penteado.pereira@escola.pr.gov.br>
2026-09-02 16:22:30 -0300

    index

--- script.js
+++ script.js
@@ -16,3 +16,17 @@
 
     }
 })
+
+const bntTemaEscuro = document.querySelector(".bnt-tema-escuro");
+
+ bntTemaEscuro.addEventListener("click", mudaTema);
+
+ function mudaTema() {
+    const corpoPagina = document.body;
+
+    if (corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
+    }
+ }
